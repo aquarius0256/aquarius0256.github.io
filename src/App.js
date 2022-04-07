b86618d5ff44956b4dcacea73f6db2ed61e0968b
aquarius0256 <aquarius0256@mail.ru>
2022-04-07 17:37:04 +0300

    anothe commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import Header from './Header.jsx';
+import Technologies from './Technologies.jsx';
 
 const App = () => {
 
