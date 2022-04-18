fc72544079293fca48e34034e296ff9be73c5057
aquarius0256 <aquarius0256@mail.ru>
2022-04-18 13:04:28 +0300

    add props (end16)

--- src/Components/Profile/MyPosts/MyPosts.jsx
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
-const MyPosts = () => {
+const MyPosts = (props) => {
     return <div>
         my posts
         <div>
@@ -10,8 +10,8 @@ const MyPosts = () => {
             <button>Add post</button>
         </div>
         <div className={s.posts}>
-            <Post/>
-            <Post/>
+            <Post message="Hi, how are you?" />
+            <Post message="It's my first post" />
         </div>
     </div>
 
--- src/Components/Profile/MyPosts/Post/Post.jsx
+++ src/Components/Profile/MyPosts/Post/Post.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import s from './Post.module.css'
-const Post = () => {
+const Post = (props) => {
     return <div>
 
         <div className={s.item}>
             <img src='https://www.meme-arsenal.com/memes/807f7063ae47455ef78d5a0743509576.jpg' />
-            post 1
+            {props.message}
         </div>
 <span>like</span>
     </div>
