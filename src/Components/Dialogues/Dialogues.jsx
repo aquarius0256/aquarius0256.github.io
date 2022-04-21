ce136cae5aee5ffa4ff1f9f711f82a71aa13588b
aquarius0256 <aquarius0256@mail.ru>
2022-04-21 13:21:14 +0300

    divide dialogues on 2 columns, add some classes (end21)

--- src/Components/Dialogues/Dialogues.jsx
+++ src/Components/Dialogues/Dialogues.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import s from './Dialogues.module.css'
+
 const Dialogues = (props) => {
-return <div>
-    Dialogues
-</div>
+    return <div className= {s.dialogues}>
+        <div className={s.dialoguesItems}>
+            <div className={s.dialog + ' ' + s.active}>
+                Artur
+            </div>
+            <div className={s.dialog}>
+                Tanyuha
+            </div>
+            <div className={s.dialog}>
+                Kamila
+            </div>
+
+        </div>
+        <div className={s.messages}>
+            <div className={s.message}> Hi</div>
+            <div className={s.message}> How are you?</div>
+            <div className={s.message}> How is your it-kamasutra?</div>
+
+        </div>
+    </div>
 }
 
 export default Dialogues;
